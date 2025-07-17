31b43720832db772730d46a1260a54b6811b07bf
Quinton <quinncg.dev@gmail.com>
2025-07-17 17:17:59 -0400

    Remade player health UI

--- Assets/Source/UI/HealthUI.cs
+++ Assets/Source/UI/HealthUI.cs
@@ -1,4 +1,6 @@
 using Sirenix.OdinInspector;
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,11 +9,58 @@ namespace Quinn.UI
 	public class HealthUI : MonoBehaviour
 	{
 		[SerializeField, Required]
-		private Slider HealthBar;
+		private Image[] Bars;
+
+		[Space]
+
+		[SerializeField, Required]
+		private Image FluidSprite;
+		[SerializeField]
+		private int FrameRate = 10;
+		[SerializeField]
+		private float MinHeight = -60f, MaxHeight = 60f;
+		[SerializeField]
+		private Sprite[] AnimSprites;
+
+		[SerializeField, Range(0f, 1f)]
+		private float DebugValue = 1f;
+
+		private int _index;
+		private float _next;
+
+		private void OnValidate()
+		{
+			Set(DebugValue);
+		}
 
 		private void LateUpdate()
 		{
-			HealthBar.value = Player.Instance.Health.Normalized;
+			float norm = Player.Instance.Health.Normalized;
+			Set(norm);
+
+			if (Time.time >= _next)
+			{
+				_next = Time.time + (1f / FrameRate);
+
+				_index++;
+
+				if (_index >= AnimSprites.Length)
+					_index = 0;
+
+				FluidSprite.sprite = AnimSprites[_index];
+			}
+		}
+
+		private void Set(float norm)
+		{
+			if (Bars.Length == 0 || FluidSprite == null)
+				return;
+
+			foreach (var bar in Bars)
+			{
+				bar.fillAmount = norm;
+				FluidSprite.rectTransform.anchoredPosition = new(0f, Mathf.Lerp(MinHeight, MaxHeight, norm));
+			}
 		}
 	}
 }
